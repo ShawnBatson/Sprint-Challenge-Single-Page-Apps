ae5c3b9385859e6310a8c9b7f68c423edae885c1
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-24 18:54:51 -0600

    adding routes to the app page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import axios from "axios";
+import { Route } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <div className="nav">
+        <Route exact path="/characters" component={CharacterList} />
+        <Route exact path="/" component={WelcomePage} />
+      </div>
     </main>
   );
 }
