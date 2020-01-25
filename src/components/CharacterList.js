c7a8d2df0b176d97c94fa3d96eb1ca7a56bc9111
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-24 18:14:24 -0600

    added the get request

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,8 +2,16 @@ import React, { useEffect, useState } from "react";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/documentation/?name=${search}")
+      .then(res => {
+        setCharacter(res.data.results).catch(err => {
+          console.log("you have encountered an error:", err);
+        });
+      });
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
